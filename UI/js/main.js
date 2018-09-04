7c1d20933d4d44c82f6253e27ee4e85e250098bd
anyatibrian <anyatibrian@gmail.com>
2018-09-04 16:42:21 +0300

    just added another branch

---
+++ UI/js/main.js
@@ -0,0 +1,19 @@
+/* begining the various modal boxes
+ */
+
+/*var orderBtn = document.getElementById('order-btn');
+var addFoodBtn = document.getElementById('add-food');
+/* various modals*/
+/*.addFoodBtn.onclick=function(){
+    alert('have been clicked');
+}
+.orderBtn.onclick=function(){
+    alert('have been clicked');
+}*/
+
+document.getElementById('add-food').onclick= function () {
+    alert('hi there');
+}
+document.getElementById('order-btn').onclick= function () {
+    alert('hi there');
+}
