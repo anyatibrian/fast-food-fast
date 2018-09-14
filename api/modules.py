c506a4060d88759fbad93305cfdfe1e926955c0a
anyatibrian <anyatibrian@gmail.com>
2018-09-14 12:04:22 +0300

    commited first changes to the branch

---
+++ api/modules.py
@@ -0,0 +1,24 @@
+order_collection = []
+
+
+class Orders:
+    def __init__(self, username, food, location, delivery_type, pieces, orderStatus='pending'):
+        self.username = username
+        self.food = food
+        self.location = location
+        self.delivery_type = delivery_type
+        self.pieces = pieces
+        self.orderStatus = orderStatus
+
+    def covert_json(self):
+        orders = {
+            "orderID": len(order_collection)+1,
+            "username": self.username,
+            "food": self.food,
+            "location": self.location,
+            "deliveryType": self.delivery_type,
+            "pieces": self.pieces,
+            "orderStatus": self.orderStatus
+        }
+        order_collection.append(orders)
+        return order_collection
---
+++ test/test_api.py
@@ -0,0 +1,43 @@
+import json
+from api import app
+
+json_data = {"username": "brian", "deliveryType": "self", "food": "fish", "location": "lira", "orderID": 4,
+             "orderStatus": "pending", "pieces": "1"}
+
+
+def test_post_orders():
+    # test case for the post request
+    with app.test_client() as test_client:
+        res = test_client.post('/orders', json=json_data)
+    res.get_json()
+    assert res.status_code == 200
+
+
+def test_get_orders():
+    # test case for the get request
+    with app.test_client() as test_client:
+        # make a post request to the page
+        get_res = test_client.post('/orders', json=json_data)
+        assert get_res.status_code == 200
+        # make a get request to the page
+        get_res = test_client.get('/orders')
+        assert get_res.status_code == 200
+
+
+def test_put_order():
+    # test case for the put request
+    with app.test_client() as test_client:
+        edit_order = test_client.post('/orders', json=json_data)
+        assert edit_order.status_code == 200
+        edit_order = test_client.put('/orders/1', json={"orderStatus": "completed"})
+        assert edit_order.status_code == 200
+
+
+def test_get_single_order():
+    # testing the put request
+    with app.test_client() as test_client:
+        get_single_order = test_client.post('/orders', json=json_data)
+        assert get_single_order.status_code == 200
+        get_single_order = test_client.get('/orders/4', follow_redirects=True)
+        assert get_single_order.status_code == 200
+        assert b'viewing single item', get_single_order.data
